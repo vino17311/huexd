ed1baff0eb670c2e73b8bf79717fafd101c94795
vino17311 <vinoth@huexd.com>
2024-06-03 23:51:46 +0530

    Update contactSchema.ts

--- schemas/contactSchema.ts
+++ schemas/contactSchema.ts
@@ -1,18 +1,19 @@
 import { z } from "zod";
 
+
 export const ContactvalidationSchema = z.object({
   firstName: z
     .string()
     .min(3, { message: "Name should be at least 3 characters long" })
     .max(50, { message: "Name should not exceed 50 characters" })
-    .regex(/^[A-Za-z]+$/, { message: "Enter characters only" })
+    .regex(/^[A-Za-z\s'-]+$/, { message: "Enter valid characters for a name" })
     .nonempty({ message: "Enter a valid first name" }),
 
   lastName: z
     .string()
     .min(3, { message: "Name should be at least 3 characters long" })
     .max(50, { message: "Name should not exceed 50 characters" })
-    .regex(/^[A-Za-z]+$/, { message: "Enter characters only" })
+    .regex(/^[A-Za-z\s'-]+$/, { message: "Enter valid characters for a name" })
     .nonempty({ message: "Enter a valid last name" }),
 
   email: z
@@ -25,7 +26,7 @@ export const ContactvalidationSchema = z.object({
 
   // phone: z
   //   .string()
-  //   .regex(/^\d{10}$/, { message: "Phone number should be 10 digits long" })
+  //   .regex(/^\d{10,15}$/, { message: "Enter a valid phone number" })
   //   .optional(),
 
   reasonForEnquiry: z
@@ -34,6 +35,8 @@ export const ContactvalidationSchema = z.object({
 
   message: z
     .string()
-    .min(20, { message: "Message should be at least 50 characters long" })
+    .min(50, { message: "Message should be at least 50 characters long" })
     .nonempty({ message: "Enter a message" }),
 });
+
+
