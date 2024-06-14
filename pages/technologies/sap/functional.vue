4bd049b17e49c4dea324d640e56fb41bd6839b60
vino17311 <vinoth@huexd.com>
2024-06-14 22:45:00 +0530

    Update functional.vue

--- pages/technologies/sap/functional.vue
+++ pages/technologies/sap/functional.vue
@@ -441,7 +441,15 @@ html {
     object-fit: cover;
 }
 
-
+.section_one_img_container {
+    width: 100%;
+    height: 390px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 13px;
+    overflow: hidden;
+}
 .c-panels__inner {
     padding: var(--inner-padding, 0 20px);
 }
